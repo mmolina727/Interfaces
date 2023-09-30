dfe3bbd579e5964db8993801e7523442cc0c99b8
mmolina727 <matiasgmolina.unicen@gmail.com>
2023-09-30 02:31:33 -0300

    Menu desplegable agregado

---
+++ TP2/js/home.js
@@ -0,0 +1,6 @@
+let burgerMain= document.getElementById("main-burger");
+let burger= document.getElementById("burger");
+
+burger.addEventListener("click",(e)=>{
+    burgerMain.classList.toggle("hidden");
+})
