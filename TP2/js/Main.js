8f160dccf6c64c08ce2c8dee3f71afa6dfe4b087
Giuliana <giuliana.delatorre@hotmail.com>
2023-10-31 16:30:33 -0300

    modifico la imagen de fondo del canva

--- TP2/js/Main.js
+++ TP2/js/Main.js
@@ -7,13 +7,11 @@ let circulos = [];
 let getCirculo = null;
 let mouseDown = false;
 
-
-
-
 const tablero = new TableroPosta(canvas, 7, 6, 70);
 
 const fondoImage = new Image();
-fondoImage.src = "https://images2.alphacoders.com/530/530934.jpg";
+fondoImage.src = "../img/fondo-tablero.jpg";
+
 
 fondoImage.onload = () => {
     // Llama a una función para dibujar el tablero con el fondo una vez que la imagen esté lista.
@@ -21,8 +19,7 @@ fondoImage.onload = () => {
     const c1 = new Ficha(50, 70, 30, "white", 0, 2 * Math.PI,ctx);
     const c2 = new Ficha(50, 200, 30, "white", 0, 2 * Math.PI,ctx);
     const c3 = new Ficha(80, 400, 30, "white", 0, 2 * Math.PI,ctx);
-    const c4 = new Ficha(80, 500, 30, "white", 0, 2 * Math.PI,ctx);
-    
+    const c4 = new Ficha(80, 500, 30, "white", 0, 2 * Math.PI,ctx);    
     
     circulos.push(c1,c2,c3,c4);
     
