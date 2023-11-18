0ffd2b43a09949db14b245d788502d8a9d459638
mmolina727 <matiasgmolina.unicen@gmail.com>
2023-11-18 05:18:36 -0300

    Parallax portada implementado

--- TP4/js/main.js
+++ TP4/js/main.js
@@ -1,11 +1,34 @@
 let burger= document.getElementById("burguer");
 let menu= document.getElementById("menu-oculto");
+let edifDer= document.getElementById("edifDer");
+let edifFrente= document.getElementById("edifFrente");
+let edifIzq= document.getElementById("edifIzq");
+let character1= document.getElementById("character1");
+let character2= document.getElementById("character2");
+let character3= document.getElementById("character3");
+let web1= document.getElementById("web1");
+let web2= document.getElementById("web2");
 
+
+window.addEventListener("scroll",()=>{
+  let scrol= document.documentElement.scrollTop;
+  edifDer.style.left=(scrol-1400)*0.09+"px";
+  edifIzq.style.right=(scrol-1400)*0.09+"px";
+  edifFrente.style.bottom=(scrol-900)*0.09+"px";
+  character1.style.top=(scrol+480)*0.20+"px";
+  character2.style.top=(scrol+630)*0.20+"px";
+  web1.style.top=(scrol+480)*0.20+"px";
+  character3.style.top=(scrol+450)*0.23+"px";
+  web2.style.top=(scrol+630)*0.23+"px";
+
+
+
+
+})
 
 burger.addEventListener("click",()=>{
     
     if(menu.classList.contains('desplegar')){
-        //burger.src="https://e7.pngegg.com/pngimages/374/900/png-clipart-computer-icons-christian-cross-icon-design-christian-cross-angle-logo.png";
         burger.src='./img/cruz-menu.png';
         menu.classList.remove('desplegar');
     }
