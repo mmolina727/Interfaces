ad5c95330b35a5275fb76c68f6112d9b99ddc88d
Giuliana <giuliana.delatorre@hotmail.com>
2023-11-17 23:54:07 -0300

    le doy un poquito de estilo al despegable del menu

--- TP4/js/main.js
+++ TP4/js/main.js
@@ -5,11 +5,47 @@ let menu= document.getElementById("menu-oculto");
 burger.addEventListener("click",()=>{
     
     if(menu.classList.contains('desplegar')){
-        burger.src="https://e7.pngegg.com/pngimages/374/900/png-clipart-computer-icons-christian-cross-icon-design-christian-cross-angle-logo.png";
+        //burger.src="https://e7.pngegg.com/pngimages/374/900/png-clipart-computer-icons-christian-cross-icon-design-christian-cross-angle-logo.png";
+        burger.src='./img/cruz-menu.png';
         menu.classList.remove('desplegar');
     }
     else{
         menu.classList.add('desplegar');
         burger.src='./img/menu-hamb.png'
+    }
+});
+
+/*====================================*/
+function showItemsWithDelay() {
+    const menuItems = document.querySelectorAll('#menu-oculto .hidden');
+    menuItems.forEach((item, index) => {
+      setTimeout(() => {
+        item.classList.add('show');
+      }, 200 * index);
+    });
+  }
+  
+  document.getElementById('burguer').addEventListener('click', () => {
+    const menuItems = document.querySelectorAll('#menu-oculto .show');
+    menuItems.forEach(item => {
+      item.classList.remove('show');
+      item.classList.add('hidden');
+    });
+  
+    // Agregar un pequeño retraso para eliminar las clases antes de volver a mostrar los elementos
+    setTimeout(() => {
+      showItemsWithDelay();
+    }, 100);
+  });
+  
+  /*******************************/
+  window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    const scrollPosition = window.scrollY;
+
+    if (scrollPosition > 0) {
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove('sticky');
     }
 });
